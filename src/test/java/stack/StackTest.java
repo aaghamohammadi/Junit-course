fcc94e46a26792ab1701a817202c32983f14a54e
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2021-09-13 17:48:23 +0430

    test: add maven junit 5 dependency and write the first dummy test

---
+++ src/test/java/stack/StackTest.java
@@ -0,0 +1,12 @@
+package stack;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StackTest {
+
+    @Test
+    public void simpleTest() {
+        Assertions.assertEquals(3,3);
+    }
+}

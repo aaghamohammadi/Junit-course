96943a38ac1e53993530f9b453489e3488373bc4
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2021-09-14 12:56:43 +0430

    test: add two tests for the Stack class

--- src/test/java/stack/StackTest.java
+++ src/test/java/stack/StackTest.java
@@ -1,12 +1,35 @@
 package stack;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StackTest {
+    private Stackable<Integer> stack;
 
+    @BeforeEach
+    public void setUp() {
+        stack = new Stack<>();
+    }
+
+    @DisplayName("Stack is empty")
     @Test
-    public void simpleTest() {
-        Assertions.assertEquals(3,3);
+    public void stackIsEmpty() {
+        Assertions.assertTrue(stack.isEmpty());
+        Assertions.assertEquals(0, stack.size());
+        Assertions.assertThrows(IllegalArgumentException.class, () -> stack.pop());
+        Assertions.assertNull(stack.peek());
+        Assertions.assertEquals("", String.valueOf(stack));
+    }
+
+    @DisplayName("Push one item")
+    @Test
+    public void pushOneItem() {
+        stack.push(2);
+        Assertions.assertFalse(stack.isEmpty());
+        Assertions.assertEquals(1, stack.size());
+        Assertions.assertEquals(2, stack.peek());
+        Assertions.assertEquals("2", String.valueOf(stack));
     }
 }
